f20eef921d0faa4838920b52f87827acdb30b03f
ligaemuya <96481698+ligaemuya@users.noreply.github.com>
2022-07-23 21:53:09 +0900

    datajpa

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -148,8 +148,11 @@ class MemberRepositoryTest {
     @Test
     public void findByNames() {
         Member m1 = new Member("AAA", 10);
+
         Member m2 = new Member("BBB", 20);
+
         memberRepository.save(m1);
+
         memberRepository.save(m2);
 
         List<Member> result = memberRepository.findByNames(Arrays.asList("AAA", "BBB"));
